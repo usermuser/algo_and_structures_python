67df8a2d6e1abd7ed616874dbf2acb417491657e
usermuser <lwrdstv@gmail.com>
2019-07-31 20:45:29 +0500

    first task is done.

--- Lesson_1/1.py
+++ Lesson_1/1.py
@@ -1,1 +1,9 @@
 # 1.	Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
+user_input = int(input('Please type three-digit number: '))
+
+first_digit = user_input // 100
+second_digit = (user_input // 10) % 10
+third_digit = user_input % 10
+
+print(f'Sum of numbers: {first_digit + second_digit + third_digit}')
+print(f'Result of multiplication of numbers: {first_digit * second_digit * third_digit}')
