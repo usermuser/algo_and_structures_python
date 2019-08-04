b2ed23fdaa257a45252f750c1e54e73768bd098a
usermuser <lwrdstv@gmail.com>
2019-08-04 19:42:26 +0500

    Lesson2: task 1,2,3 done (task 3 also with recursion)

--- Lesson_2/3.py
+++ Lesson_2/3.py
@@ -3,3 +3,25 @@
 цифр и вывести на экран. Например, если введено число 3486,
  то надо вывести число 6843.
 """
+
+num = int(input('Please enter a number: '))
+
+def rev(num):
+    s = ''
+    while num > 0:
+        s += str(num%10)
+        num = num // 10
+    return s
+
+print(f'Cycle result: {rev(num)}')
+
+
+def recursion(num, s=''):
+    if num < 1:
+        return s
+    s += str(num%10)
+    return recursion((num // 10), s)
+
+
+print(f'Recursion result: {recursion(num)}')
+
