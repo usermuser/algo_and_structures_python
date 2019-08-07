97e5b645a76c3ab8b20361f1d1e0ae0e49c7f19c
usermuser <lwrdstv@gmail.com>
2019-08-08 00:13:35 +0500

    task 1, 5 done

--- Lesson_3/1.py
+++ Lesson_3/1.py
@@ -1,2 +1,25 @@
 # 1.	В диапазоне натуральных чисел от 2 до 99 определить,
 # сколько из них кратны каждому из чисел в диапазоне от 2 до 9.
+
+t = tuple(i for i in range(2,100))              # 2 to 99
+myrange = tuple(i for i in range(2,10))         # 2 to 9
+c = 0    # our counter
+
+
+def checker(num,myrange):
+    _flag = False         # indicator that our number is a multiple of the entire range of numbers in myrange
+    for i in myrange:
+        if num % i == 0:
+            _flag = True
+        else:
+            _flag = False
+    return _flag
+
+
+# Let's check all numbers in tuple t
+for i in t:
+    if checker(i,myrange):
+        c += 1
+
+print(f'So, {c} numbers are multiple of each number in range 2 to 9')
+
