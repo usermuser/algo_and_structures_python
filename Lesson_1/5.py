4992b0bfb74d0777d5cd5ee5e1cfe5f61f934902
usermuser <lwrdrstv@gmail.com>
2019-11-26 16:42:54 +0500

    fixed bug in lesson 5 when position of second letter is bigger than position of fitst letter. (other cases we don't have to check on this lesson)

--- Lesson_1/5.py
+++ Lesson_1/5.py
@@ -1,2 +1,19 @@
 #5.	Пользователь вводит две буквы. Определить, на каких местах
 # алфавита они стоят, и сколько между ними находится букв.
+
+letter1 = input('Please input any latin letter: ')
+letter2 = input('Please input any latin letter: ')
+
+# to get position of letter we need to convert it to ascii code and substitute the offset
+ascii_offset = 96
+pos1 = ord(letter1) - ascii_offset
+pos2 = ord(letter2) - ascii_offset
+
+print(f'position of \'{letter1}\' is {pos1} and position of \'{letter2}\' is {pos2}')
+
+if pos2 > pos1:
+    print(f'between them we see {pos2 - pos1} other letters')
+else:
+    print(f'between them we have {pos1 - pos2} other letters')
+
+
