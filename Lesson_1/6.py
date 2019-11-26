a07675bba2335cd3d84887eeefc1b4ca039ad555
usermuser <lwrdrstv@gmail.com>
2019-11-26 17:10:46 +0500

    task6 - done correctly

--- Lesson_1/6.py
+++ Lesson_1/6.py
@@ -1,4 +1,5 @@
 # 6.	Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
 
 num = int(input('Please enter a number: '))
-print(f'Corresponding letter for this number is \'{chr(num)}\'')
+ascii_num = num + ord('a') - 1
+print(f'Corresponding letter for this number is \'{chr(ascii_num)}\'')
