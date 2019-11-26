f9f1258e618a10a032de7d8c894c24ec345ee09d
usermuser <lwrdrstv@gmail.com>
2019-11-26 16:14:38 +0500

    task 3 done

--- Lesson_1/3.py
+++ Lesson_1/3.py
@@ -1,2 +1,15 @@
 # 3.	По введенным пользователем координатам двух точек вывести
 # уравнение прямой вида y = kx + b, проходящей через эти точки.
+
+x1 = int(input('Please, type X coordinate for first point: '))
+y1 = int(input('Please, enter Y coordinate for first point: '))
+x2 = int(input('Please, enter X coordinate for second point: '))
+y2 = int(input('Please, enter Y coordinate for second point: '))
+
+k = (y2-y1) / (x2-x1)
+b = y2 - k*x2
+
+print(f'Уравнения прямой для этих точек = y = {k}x+{b}')
+
+
+
